cbb057ec96c55a352981fb1920d251004e41d710
Eric Shtivelberg <295836+shedokan@users.noreply.github.com>
2023-09-17 05:11:52 +0300

    WIP: Auto move drag

--- src/extensions/right-to-left/client.mjs
+++ src/extensions/right-to-left/client.mjs
@@ -8,6 +8,49 @@
 'use strict';
 
 export default async function ({ web }, db) {
+  //observer && observer.disconnect();
+
+const dragFrame = $('#notion-app > div > div:nth-child(1) > div > div:nth-child(2) > main > div > div:nth-child(1)'),
+      notionScroller = dragFrame?.parentElement;
+
+function logChanges(mutationList, observer) {
+    console.log(mutationList.map((m) => !m.addedNodes.length));
+    if(mutationList.every((m) => !m.addedNodes.length)) return;
+    
+    const dragHandle = document.querySelector('div[aria-label="Drag"]')?.parentElement,
+      dragContainer = dragHandle?.parentElement?.parentElement,
+      dataBlockId = dragHandle?.getAttribute('data-block-id'),
+      newRight = (notionScroller.clientWidth + parseInt(dragContainer.style.left)) + 'px';;
+    //dataBlockId && document.querySelectorAll('div[data-block-id="' + dataBlockId + '"]')[1];
+    dragContainer.style.right = newRight;
+    dragContainer.style.left = '';
+    console.log(`Updated right to: ${newRight}`);
+    console.log(mutationList)
+
+  // for (const record of mutationList) {
+  //   for (const addedNode of record.addedNodes) {
+  //     console.log(`Added: ${addedNode.textContent}`);
+  //   }
+  //   for (const removedNode of record.removedNodes) {
+  //     console.log(`Removed: ${removedNode.textContent}`);
+  //   }
+  //   if (record.target.childNodes.length === 0) {
+  //     console.log(`Disconnected`);
+  //     observer.disconnect();
+  //   }
+  //   console.log(record.target.childNodes.length);
+  // }
+}
+
+const observer = new MutationObserver(logChanges);
+observer.observe(dragFrame.querySelector('\div'), {
+  childList: true,
+  // subtree: true,
+});
+
+
+
+  
   const pageContentSelector = `
       .notion-page-content >
         div[data-block-id]:not([dir]):not(.notion-column_list-block):not(.notion-collection_view_page-block),
@@ -40,6 +83,7 @@ export default async function ({ web }, db) {
       }
     });
   };
+  
   web.addDocumentObserver(autoAlignText, [
     pageContentSelector,
     listItemSelector,
